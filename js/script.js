9b5102f5f8fd483fb1901ca860bc3acb495573cb
marjorii <hello@marjorieober.com>
2019-11-26 21:05:44 +0100

    Add map (without style)

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ document.querySelector('#head nav a').onclick = function(e) {
     e.preventDefault();
     document.getElementById('menu').classList.remove('hide');
 }
-document.getElementById('cross').onclick = function(e) {
+document.querySelector('#menu #cross').onclick = function(e) {
     e.preventDefault();
     document.getElementById('menu').classList.add('hide');
+}
+document.querySelector(' #menu #map-toggler').onclick = function(e) {
+    e.preventDefault();
+    document.getElementById('map').classList.remove('hide');
+}
+document.querySelector('#map #cross').onclick = function(e) {
+    e.preventDefault();
+    document.getElementById('map').classList.add('hide');
 }
