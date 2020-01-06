5fe044e6d9905f0d5a34330a2c910de521a11ccc
marjorii <hello@marjorieober.com>
2020-01-06 21:15:27 +0100

    Quickly set error page + style content page (responsive as well)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+if (document.body.classList.contains('content')) {
+    document.querySelector('#list nav #menu-toggler').onclick = function(e) {
+        e.preventDefault();
+        document.getElementById('menu').classList.remove('hide');
+    }
+    document.querySelector('#menu #cross').onclick = function(e) {
+        e.preventDefault();
+        document.getElementById('menu').classList.add('hide');
+    }
+    document.querySelector('#list nav #map-toggler').onclick = function(e) {
+        e.preventDefault();
+        document.getElementById('map').classList.remove('hide');
+    }
+    document.querySelector('#map #cross').onclick = function(e) {
+        e.preventDefault();
+        document.getElementById('map').classList.add('hide');
+    }
+}
 document.getElementById('menu-toggler').onclick = function(e) {
     e.preventDefault();
     document.getElementById('menu').classList.remove('hide');
